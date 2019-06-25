71e1c1803696dcd5f7ba3de6fb924c2e1228f221
Kai Zhao <kaizhao@Kais-MacBook-Pro.local>
2019-06-24 21:58:24 -0700

    updated backend up to possibly not be main

--- top_reddit_backend.py
+++ top_reddit_backend.py
@@ -97,25 +97,29 @@ def test_case(cursor):
     print("test case 8: should print 2 things")
     print(get_results(cursor, 3))
 
-try:
-    connection = psycopg2.connect(user = "allen",
-                                  password = "123123qwer",
-                                  host = "104.155.165.207",
-                                  port = "5432",
-                                  database = "reddit_db")
-    cursor = connection.cursor()
-    # Print PostgreSQL Connection properties
-    print(connection.get_dsn_parameters(),"\n")
-    # Print PostgreSQL version
-    cursor.execute("SELECT version();")
-    record = cursor.fetchone()
-    print("You are connected to - ", record,"\n")
-    test_case(cursor)
-except (Exception, psycopg2.Error) as error :
-    print ("Error while connecting to PostgreSQL", error)
-finally:
-    #closing database connection.
-    if(connection):
-        cursor.close()
-        connection.close()
-        print("PostgreSQL connection is closed")
+def main():
+    try:
+        connection = psycopg2.connect(user = "allen",
+                                    password = "123123qwer",
+                                    host = "104.155.165.207",
+                                    port = "5432",
+                                    database = "reddit_db")
+        cursor = connection.cursor()
+        # Print PostgreSQL Connection properties
+        print(connection.get_dsn_parameters(),"\n")
+        # Print PostgreSQL version
+        cursor.execute("SELECT version();")
+        record = cursor.fetchone()
+        print("You are connected to - ", record,"\n")
+        test_case(cursor)
+    except (Exception, psycopg2.Error) as error :
+        print ("Error while connecting to PostgreSQL", error)
+    finally:
+        #closing database connection.
+        if(connection):
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
+
+if __name__ == '__main__':
+    main()
