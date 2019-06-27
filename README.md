4bf8a4a79634a05f1794e2fdef1346f9bb44b2cf
allenjhuang <allenjhuang@outlook.com>
2019-06-27 02:06:45 -0500

    Commented code!

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 # top_reddit
 
 TODO:
-clean up top_reddit_scape.py and add more comments
-
-write code to periodically run the scapper
-
 write the front end
+
--- top_reddit_scrape.py
+++ top_reddit_scrape.py
@@ -13,27 +13,26 @@ else:
 
 import top_reddit_backend
 
+# get list of entries from url using BeautifulSoup.
 def get_entries(reddit_rss_url):
+    # keep trying to read page until we find >0 entries.
     while True:
         try:
             url_client = urlopen(reddit_rss_url)
             page_html = url_client.read()
             url_client.close()
-            #with urlopen(reddit_rss_url) as url_client:
-            #    page_html = url_client.read()
-            # html parsing
             page_soup = BeautifulSoup(page_html, "html.parser")
-            # find 25 entries
             entries = page_soup.findAll("entry");
             if len(entries) > 0:
                 return entries
-        except Exception:
+        except Exception: # reddit returns 0 entries if scraping too often to avoid bots.
             print("warning: failed to fetch page, retrying in 2 seconds; ", end=" ")
             time.sleep(2)
 
+# main, used to connect to postgres server, scrape, update table, and commit.
 def main():
-    num_docs_remaining_to_scrape = 100
-    print(str(datetime.datetime.now()) + '; ', end=" ")
+    num_entries_remaining_to_scrape = 100 # decrease if want only higher quality; increase if want to scrape low quality or r/new.
+    print(str(datetime.datetime.now()) + '; ', end=" ") # log time.
     reddit_rss_url = "https://reddit.com/.rss"
 
     # Connect to SQL server.
@@ -46,17 +45,21 @@ def main():
         cursor = connection.cursor()
         print("PostgreSQL connection is opened; ", end=" ")
 
-        # create table if it doesn't already exists
+        # create table if it doesn't already exists.
         if not top_reddit_backend.is_table_exists(cursor):
             top_reddit_backend.create_table(cursor)
 
+        # loop to scrap reddit entries
         post_id = ""
         rank_counter = 0
-        while num_docs_remaining_to_scrape > 0:
+        while num_entries_remaining_to_scrape > 0:
+            # call function to get entries using rss url
             if rank_counter == 0:
                 entries = get_entries(reddit_rss_url)
             else:
+                # if we need to go the next page, then append '?after=' + post_id of last entry of this page
                 entries = get_entries(reddit_rss_url + '?after=' + post_id)
+            # loop through each entry to scrap content
             for entry in entries:
                 author_name = entry.findAll("name")[0].text
                 category = entry.findAll("category")[0]['term']
@@ -80,9 +83,9 @@ def main():
                 # add record into table
                 top_reddit_backend.modify_record(cursor, post_id, rank_counter+1, category, title, link, author_name, updated, content)
                 rank_counter = rank_counter + 1
-            num_docs_remaining_to_scrape = num_docs_remaining_to_scrape - len(entries)
+            num_entries_remaining_to_scrape = num_entries_remaining_to_scrape - len(entries)
 
-        connection.commit()
+        connection.commit() # commit update to sql database
     
     except (Exception, psycopg2.Error) as error :
         print("Error while connecting to PostgreSQL; ", error, end=" ")
