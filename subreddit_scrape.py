c1b8cbc3037468fbead63f59636cc2a680ef8b3a
kzhao32 <kzhao32@cs.wisc.edu>
2019-08-04 04:37:53 +0000

    before aug 3rd update

--- subreddit_scrape.py
+++ subreddit_scrape.py
@@ -14,13 +14,15 @@ else:
 
 import top_reddit_backend
 
-TABLE_NAME = "top_posts"
+TABLE_NAME = "subreddits"
 
 
 # Get list of entries from url using BeautifulSoup.
 def get_entries(reddit_rss_url):
     # Keep trying to read page until we find >0 entries.
-    while True:
+    tries = 10
+    while tries > 0:
+        tries -= 1
         try:
             url_client = urlopen(reddit_rss_url)
             page_html = url_client.read()
@@ -32,12 +34,13 @@ def get_entries(reddit_rss_url):
         except Exception: # Reddit returns 0 entries if scraping too often to avoid bots.
             print("warning: failed to fetch page, retrying in 2 seconds; ", end=" ")
             time.sleep(2)
+    return
 
 
 # main: used to connect to postgres server, scrape, update table, and commit.
 def main():
-    num_entries_remaining_to_scrape = 100 # decrease if want only higher quality; increase if want to scrape low quality or r/new.
     print(str(datetime.datetime.now()) + '; ', end=" ") # log time.
+
     reddit_rss_url = "https://reddit.com/.rss"
 
     # Connect to SQL server.
@@ -54,19 +57,24 @@ def main():
         if not top_reddit_backend.is_table_exists(cursor, TABLE_NAME):
             top_reddit_backend.create_table(cursor, TABLE_NAME)
 
-        # Loop to scrape reddit entries.
+        # Scrape every subreddit of posts that reach front page.
         post_id = ""
-        rank_counter = 0
-        while num_entries_remaining_to_scrape > 0:
+        subreddits = top_reddit_backend.get_subreddits(cursor, "top_posts")
+        subreddits_counter = 0
+        for subreddit in subreddits:
+            print(str(subreddits_counter) + ": scraping " + subreddit[0] + ".")
+            subreddits_counter += 1
+            rank_counter = 0
             # Call function to get entries using rss url.
-            if rank_counter == 0:
-                entries = get_entries(reddit_rss_url)
-            else:
-                # If we need to go the next page, then append '?after=' + post_id of last entry of this page
-                entries = get_entries(reddit_rss_url + '?after=' + post_id)
+            entries = get_entries("https://reddit.com/r/" + subreddit[0] + "/.rss")
+            # Skip unsuccessful return value, such as /r/FBIOpenUp.
+            if entries is None:
+                continue
             # Loop through each entry to scrape content.
             for entry in entries:
-                author_name = entry.findAll("name")[0].text
+                author_name = "/u/[deleted]"
+                if len(entry.findAll("name")) > 0:
+                    author_name = entry.findAll("name")[0].text
                 category = entry.findAll("category")[0]['term']
                 post_id = entry.findAll("id")[0].text
                 link = entry.findAll("link")[0]['href']
@@ -85,21 +93,26 @@ def main():
                 # print("content = " + content)
                 # print("~~~Entry end~~~~~~~~~~~~~~~~~~~~~~~\n")
 
-                # Add record into table.
+                # Add entry as a record into table.
                 top_reddit_backend.modify_record(cursor, TABLE_NAME, post_id, rank_counter+1, category, title, link, author_name, updated, content)
                 rank_counter = rank_counter + 1
-            num_entries_remaining_to_scrape = num_entries_remaining_to_scrape - len(entries)
-
-        connection.commit() # commit update to sql database
+            # Commit updates to database every 25 subreddits.
+            if (subreddits_counter % 25 == 0):
+                print("Committing to database now.***************")
+                connection.commit()
+        # Do a final commit to database after all subreddits.
+        print("Committing to database now.***************")
+        connection.commit()
     
     except (Exception, psycopg2.Error) as error :
         print("Error while connecting to PostgreSQL; ", error, end=" ")
     finally:
-        # Closing database connection.
+        # Close database connection.
         if(connection):
             cursor.close()
             connection.close()
             print("PostgreSQL connection is closed; ", end=" ")
+    print(str(datetime.datetime.now()) + '; ', end=" ") # log time.
     print("Done with scrape;")
 
 
--- top_reddit_backend.py
+++ top_reddit_backend.py
@@ -1,14 +1,14 @@
 import psycopg2
 
-TABLE_NAME = "top_posts"
 
-def is_table_exists(cursor):
-    cursor.execute("SELECT to_regclass('" + TABLE_NAME + "');")
-    return cursor.fetchone()[0] == TABLE_NAME
+def is_table_exists(cursor, table_name):
+    cursor.execute("SELECT to_regclass('" + table_name + "');")
+    return cursor.fetchone()[0] == table_name
 
-def create_table(cursor):
+
+def create_table(cursor, table_name):
     cursor.execute( \
-        "CREATE TABLE " + TABLE_NAME + "(" \
+        "CREATE TABLE " + table_name + "(" \
             "post_id text NOT NULL," \
             "top_rank smallint NOT NULL," \
             "time_top_rank_achieved timestamptz NOT NULL," \
@@ -21,24 +21,35 @@ def create_table(cursor):
         ");" \
     )
 
-def get_record(cursor, post_id):
+
+def get_record(cursor, table_name, post_id):
     cursor.execute( \
         "SELECT * " \
-        "FROM " + TABLE_NAME + " " \
+        "FROM " + table_name + " " \
         "WHERE post_id = '" + post_id + "';" \
     )
     return cursor.fetchone()
 
-def get_results(cursor, threshold_rank):
+
+def get_results(cursor, table_name, threshold_rank):
     cursor.execute( \
         "SELECT * " \
-        "FROM " + TABLE_NAME + " " \
+        "FROM " + table_name + " " \
         "WHERE top_rank <= " + str(threshold_rank) + " " \
         "ORDER BY time_top_rank_achieved DESC;"
     )
     return cursor.fetchall()
 
-def modify_record(cursor, post_id, rank, category, title, link, author_name, updated, content):
+
+def get_subreddits(cursor, table_name):
+    cursor.execute( \
+        "SELECT DISTINCT category " \
+        "FROM " + table_name + ";"
+    )
+    return cursor.fetchall()
+
+
+def modify_record(cursor, table_name, post_id, rank, category, title, link, author_name, updated, content):
     # add record
     # commit
     # call get_record
@@ -50,54 +61,56 @@ def modify_record(cursor, post_id, rank, category, title, link, author_name, upd
         # if existing rank > record rank
             # update record
             # commit
-    lookup = get_record(cursor, post_id)
+    lookup = get_record(cursor, table_name, post_id)
     if lookup == None:
         cursor.execute("SELECT NOW();")
         time_top_rank_achieved = str(cursor.fetchone()[0])
         cursor.execute( \
-            "INSERT INTO " + TABLE_NAME + " " \
+            "INSERT INTO " + table_name + " " \
             "(post_id, top_rank, time_top_rank_achieved, category, title, link, author_name, updated, content) " \
             "VALUES ('" + post_id + "'," + str(rank) + ",'" + time_top_rank_achieved + "','" + category + "','" + title + "','" + link + "','" + author_name + "','" + updated + "','" + content + "');" \
         )
     else:
         if lookup[1] > rank: # if existing_in_table < current_rank_to_maybe_put_in_table
             cursor.execute( \
-                "UPDATE " + TABLE_NAME + " " \
+                "UPDATE " + table_name + " " \
                 "SET top_rank = " + str(rank) + " " \
                 "WHERE post_id = '" + post_id + "';"
             )
 
-def test_case(cursor):
+
+def test_case(cursor, table_name):
     print("setup 0")
-    modify_record(cursor, "tifu_qwe", 1, "tifu", "tifu somehow", "google.com", "me", '2019-06-14 19:10:25-07', 'my content')
+    modify_record(cursor, table_name, "tifu_qwe", 1, "tifu", "tifu somehow", "google.com", "me", '2019-06-14 19:10:25-07', 'my content')
     print("setup 1")
-    modify_record(cursor, "jokes_abc", 5, "jokes", "6 afraid of 7", "youtube.com" "you", '2016-06-22 19:10:25-07', 'your content')
+    modify_record(cursor, table_name, "jokes_abc", 5, "jokes", "6 afraid of 7", "youtube.com" "you", '2016-06-22 19:10:25-07', 'your content')
     print("test case 1")
-    print(get_record(cursor, "tifu_qwe"))
+    print(get_record(cursor, table_name, "tifu_qwe"))
     print("test case 2")
-    print(get_record(cursor, "jokes_abc"))
+    print(get_record(cursor, table_name, "jokes_abc"))
     print("test case 3")
-    print(get_record(cursor, "should not exist"))
+    print(get_record(cursor, table_name, "should not exist"))
     print("setup 2")
-    modify_record(cursor, "tifu_qwe", 3, "tifu", "tifu somehow", "facebook.com", "me", '2019-06-14 19:10:25-07', 'her content')
-    record_to_check = get_record(cursor, "tifu_qwe")
+    modify_record(cursor, table_name, "tifu_qwe", 3, "tifu", "tifu somehow", "facebook.com", "me", '2019-06-14 19:10:25-07', 'her content')
+    record_to_check = get_record(cursor, table_name, "tifu_qwe")
     if record_to_check[1] == 1:
         print("test case 4 passed")
     else:
         print("test case 4 failed")
     print("setup 3")
-    modify_record(cursor, "jokes_abc", 3, "jokes", "6 afraid of 7", "amazon.com", "you", '2016-06-22 19:10:25-07', 'his content')
-    record_to_check = get_record(cursor, "jokes_abc")
+    modify_record(cursor, table_name, "jokes_abc", 3, "jokes", "6 afraid of 7", "amazon.com", "you", '2016-06-22 19:10:25-07', 'his content')
+    record_to_check = get_record(cursor, table_name, "jokes_abc")
     if record_to_check[1] == 3:
         print("test case 5 passed")
     else:
         print("test case 5 failed")
     print("test case 6: should print nothing")
-    print(get_results(cursor, 0))
+    print(get_results(cursor, table_name, 0))
     print("test case 7: should print 1 thing")
-    print(get_results(cursor, 2))
+    print(get_results(cursor, table_name, 2))
     print("test case 8: should print 2 things")
-    print(get_results(cursor, 3))
+    print(get_results(cursor, table_name, 3))
+
 
 def main():
     try:
@@ -113,7 +126,7 @@ def main():
         cursor.execute("SELECT version();")
         record = cursor.fetchone()
         print("You are connected to - ", record,"\n")
-        test_case(cursor)
+        test_case(cursor, "test_table")
     except (Exception, psycopg2.Error) as error :
         print ("Error while connecting to PostgreSQL", error)
     finally:
@@ -122,6 +135,7 @@ def main():
             cursor.close()
             connection.close()
             print("PostgreSQL connection is closed")
+
 
 if __name__ == '__main__':
     main()
